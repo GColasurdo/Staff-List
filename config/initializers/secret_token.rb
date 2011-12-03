dd51e24fa0d9bfd9a129394eeea7069a25031215
Garth Colasurdo <garth.colasurdo@gmail.com>
2011-12-03 16:40:12 -0700

    New rails app for staff list

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StaffList::Application.config.secret_token = '723782b8737e96a6498d13187e44516ca726bbd0918535e20da6504fd7e4e0a40dcae3ef3a933fc1b784a8cb2833c5165da4da75febaecf83678e4f7cf566def'
